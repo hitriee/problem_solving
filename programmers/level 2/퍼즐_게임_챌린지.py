2b5828cb195d11c80d91fc1c16860d8dd76a33cc
pubhan35@gmail.com <pubhan35@gmail.com>
2024-12-11 16:23:32 +0900

    [PGR] 퍼즐_게임_챌린지

--- programmers/level 2/퍼즐_게임_챌린지.py
+++ programmers/level 2/퍼즐_게임_챌린지.py
@@ -98,3 +98,56 @@ def solution(diffs, times, limit):
 테스트 20 〉	통과 (335.52ms, 34.9MB)
 테스트 21 〉	통과 (1532.41ms, 35MB)
 '''
+
+
+
+#
+def solution(diffs, times, limit):
+    # 난이도 배열, 소요 시간 배열, 제한 시간
+    N = len(diffs)
+
+    # 이분탐색
+    start, end = 1, max(diffs)
+    while start <= end:
+        mid = (start + end) // 2
+        total = times[0]
+        for i in range(1, N):
+            if total > limit:
+                start = mid + 1
+                break
+            diff = diffs[i]
+            if diff > mid:
+                total += (diff - mid) * (times[i - 1] + times[i])
+            total += times[i]
+        else:
+            if total > limit:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+    return start
+
+'''
+정확성  테스트
+테스트 1 〉	통과 (0.01ms, 10.2MB)
+테스트 2 〉	통과 (0.01ms, 10.3MB)
+테스트 3 〉	통과 (0.01ms, 10MB)
+테스트 4 〉	통과 (0.04ms, 10.3MB)
+테스트 5 〉	통과 (0.03ms, 10.2MB)
+테스트 6 〉	통과 (0.03ms, 10.3MB)
+테스트 7 〉	통과 (0.02ms, 10.1MB)
+테스트 8 〉	통과 (3.25ms, 10.2MB)
+테스트 9 〉	통과 (4.38ms, 10.3MB)
+테스트 10 〉	통과 (2.00ms, 10MB)
+테스트 11 〉	통과 (4.05ms, 10.1MB)
+테스트 12 〉	통과 (3.06ms, 10.3MB)
+테스트 13 〉	통과 (3.59ms, 10.4MB)
+테스트 14 〉	통과 (4.33ms, 10.1MB)
+테스트 15 〉	통과 (1053.74ms, 35.2MB)
+테스트 16 〉	통과 (1003.78ms, 34.9MB)
+테스트 17 〉	통과 (678.76ms, 34.7MB)
+테스트 18 〉	통과 (862.54ms, 34.4MB)
+테스트 19 〉	통과 (1468.79ms, 34MB)
+테스트 20 〉	통과 (337.67ms, 34.7MB)
+테스트 21 〉	통과 (1402.20ms, 34.7MB)
+'''
