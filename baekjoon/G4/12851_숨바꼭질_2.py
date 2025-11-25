a5512fc8490179443732fae9219f838fabf961c3
pubhan35@gmail.com <pubhan35@gmail.com>
2025-11-25 20:43:56 +0900

    [BOJ] 12851_숨바꼭질_2

--- baekjoon/G4/12851_숨바꼭질_2.py
+++ baekjoon/G4/12851_숨바꼭질_2.py
@@ -95,3 +95,55 @@ def main():
 
 
 print(main())
+
+
+
+
+
+# 40076 KB / 108 ms
+def main():
+    from collections import deque
+
+    N, K = map(int, input().split())
+
+    LIMIT = max(int(1e5), 2 * N) + 1
+
+    time = [int(1e5)] * LIMIT
+    cnt = [0] * LIMIT
+    time[K], cnt[K] = 0, 1
+
+    q = deque()
+    q.append((K, 0))
+
+    while q:
+        position, duration = q.popleft()
+        if position == N:
+            continue
+
+        new_duration = duration + 1
+        new_positions = [position - 1, position + 1]
+
+        if position % 2 == 0:
+            new_positions.append(position // 2)
+
+        for new_position in new_positions:
+            if new_position < 0 or new_position >= LIMIT:
+                continue
+
+            each_time = time[new_position]
+            if each_time < new_duration:
+                continue
+
+            elif each_time > new_duration:
+                time[new_position] = new_duration
+                cnt[new_position] = 1
+
+            else:
+                cnt[new_position] += 1
+
+            q.append((new_position, new_duration))
+
+    return '\n'.join([str(time[N]), str(cnt[N])])
+
+
+print(main())
