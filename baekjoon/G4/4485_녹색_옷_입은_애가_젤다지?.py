d830cf8f6d2277b23307d79e534b86a854c1b82b
pubhan35@gmail.com <pubhan35@gmail.com>
2025-12-17 10:28:56 +0900

    [BOJ] 4485_녹색_옷_입은_애가_젤다지?

--- baekjoon/G4/4485_녹색_옷_입은_애가_젤다지?.py
+++ baekjoon/G4/4485_녹색_옷_입은_애가_젤다지?.py
@@ -92,3 +92,151 @@ def main():
 
 
 main()
+
+
+
+# 35508 KB / 84 ms
+def main():
+    from heapq import heappush, heappop
+    from sys import stdin
+
+    tc = 1
+    new_input = stdin.readline
+    delta_arr = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    answer, heap = [], []
+
+    while True:
+        N = int(new_input())
+        if N == 0:
+            return '\n'.join(answer)
+
+        def in_range(idx):
+            return 0 <= idx < N
+
+        limit = int(1e6)
+        arr = [list(map(int, new_input().split())) for _ in range(N)]
+        min_lost = [[limit] * N for _ in range(N)]
+
+        heappush(heap, (arr[0][0], 0, 0))
+        min_lost[0][0] = arr[0][0]
+
+        while heap:
+            lost, y, x = heappop(heap)
+            if min_lost[y][x] < lost:
+                continue
+            if y == N-1 and x == N-1:
+                heap.clear()
+                answer.append(f'Problem {tc}: {min_lost[-1][-1]}')
+                tc += 1
+                break
+
+            for dy, dx in delta_arr:
+                ny, nx = y+dy, x+dx
+                if in_range(ny) and in_range(nx):
+                    new_lost = lost + arr[ny][nx]
+                    if min_lost[ny][nx] <= new_lost:
+                        continue
+                    min_lost[ny][nx] = new_lost
+                    heappush(heap, (new_lost, ny, nx))
+
+
+print(main())
+
+
+
+
+# 35508 KB / 84 ms
+def main():
+    from heapq import heappush, heappop
+    from sys import stdin
+
+    tc = 1
+    new_input = stdin.readline
+    delta_arr = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    answer = []
+
+    while True:
+        N = int(new_input())
+        if N == 0:
+            return '\n'.join(answer)
+
+        def in_range(idx):
+            return 0 <= idx < N
+
+        limit = int(1e6)
+        heap = []
+        arr = [list(map(int, new_input().split())) for _ in range(N)]
+        min_lost = [[limit] * N for _ in range(N)]
+
+        heappush(heap, (arr[0][0], 0, 0))
+        min_lost[0][0] = arr[0][0]
+
+        while heap:
+            lost, y, x = heappop(heap)
+            if min_lost[y][x] < lost:
+                continue
+            if y == N-1 and x == N-1:
+                answer.append(f'Problem {tc}: {min_lost[-1][-1]}')
+                tc += 1
+                break
+
+            for dy, dx in delta_arr:
+                ny, nx = y+dy, x+dx
+                if in_range(ny) and in_range(nx):
+                    new_lost = lost + arr[ny][nx]
+                    if min_lost[ny][nx] <= new_lost:
+                        continue
+                    min_lost[ny][nx] = new_lost
+                    heappush(heap, (new_lost, ny, nx))
+
+
+print(main())
+
+
+
+
+# 35508 KB / 80 ms
+def main():
+    from heapq import heappush, heappop
+    from sys import stdin
+
+    tc = 1
+    new_input = stdin.readline
+    delta_arr = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    answer = []
+
+    while True:
+        N = int(new_input())
+        if N == 0:
+            return '\n'.join(answer)
+
+        def in_range(idx):
+            return 0 <= idx < N
+
+        limit = int(1e6)
+        heap = []
+        arr = [list(map(int, new_input().split())) for _ in range(N)]
+        min_lost = [[limit] * N for _ in range(N)]
+
+        heappush(heap, (arr[0][0], 0, 0))
+        min_lost[0][0] = arr[0][0]
+
+        while heap:
+            lost, y, x = heappop(heap)
+            if min_lost[y][x] < lost:
+                continue
+            if y == N-1 and x == N-1:
+                answer.append(f'Problem {tc}: {min_lost[-1][-1]}')
+                tc += 1
+                break
+
+            for dy, dx in delta_arr:
+                ny, nx = y+dy, x+dx
+                if in_range(ny) and in_range(nx):
+                    new_lost = lost + arr[ny][nx]
+                    if min_lost[ny][nx] > new_lost:
+                        min_lost[ny][nx] = new_lost
+                        heappush(heap, (new_lost, ny, nx))
+
+
+print(main())
