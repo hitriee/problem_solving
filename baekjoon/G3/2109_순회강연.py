e12e214edac8199deb760520649db6420ffcbecc
pubhan35@gmail.com <pubhan35@gmail.com>
2026-01-13 20:34:56 +0900

    [BOJ] 2109_순회강연

--- baekjoon/G3/2109_순회강연.py
+++ baekjoon/G3/2109_순회강연.py
@@ -71,3 +71,81 @@ def main():
     return total
 
 print(main())
+
+
+
+
+# 36532 KB / 72 ms
+def main():
+    from sys import stdin
+    from heapq import heappush, heappop
+
+    new_input = stdin.readline
+    N = int(new_input())
+    heap = []
+    for _ in range(N):
+        p, d = map(int, new_input().split())
+        heappush(heap, (-p, d))
+
+    total = 0
+    i_list = list(range(1, 10001))
+
+    while heap:
+        income, limit = heappop(heap)
+        start, end = 0, len(i_list) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+            mid_i = i_list[mid]
+            if mid_i < limit:
+                start = mid + 1
+            elif mid_i > limit:
+                end = mid - 1
+            else:
+                end = mid
+                break
+
+        if end >= 0:
+            total -= income
+            i_list.pop(end)
+
+    return total
+
+print(main())
+
+
+
+
+# 36532 KB / 72 ms
+def main():
+    from sys import stdin
+    from heapq import heappush, heappop
+
+    new_input = stdin.readline
+    N = int(new_input())
+    heap = []
+    for _ in range(N):
+        p, d = map(int, new_input().split())
+        heappush(heap, (-p, d))
+
+    total = 0
+    i_list = list(range(1, 10001))
+
+    while heap:
+        income, limit = heappop(heap)
+        start, end = 0, len(i_list) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+            if i_list[mid] <= limit:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+        if end >= 0:
+            total -= income
+            i_list.pop(end)
+
+    return total
+
+print(main())
